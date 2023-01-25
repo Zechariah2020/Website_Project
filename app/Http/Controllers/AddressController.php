ed3c50daacf43ed3ee2ee13772a31bb66a9dce34
Charlotte!2023 <MichaelScofield2015@Yahoo.Com>
2023-01-25 16:02:09 +0330

    a few validation rules added for storing and updating the addresses.

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\AddressResource;
 use Illuminate\Http\Request;
 use App\Models\Address;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AddressController extends Controller
 {
@@ -29,41 +30,79 @@ class AddressController extends Controller
 
     public function store(Request $request)
     {
-        $user_id = Auth::user()->id;
-        $address = new Address;
-        $address->user_id = $user_id;
-        $address->description = $request->description;
-        $address->province = $request->province;
-        $address->city = $request->city;
-        $result = $address->save();
-        if ($result) {
+        try {
+            $user_id = Auth::user()->id;
+            $validated = Validator::make($request->all(), [
+                'description' => 'required|string|max:255',
+                'province' => 'required|string|max:255',
+                'city' => 'required|string|max:255'
+            ]);
+
+            if ($validated->fails()) {
+                $error = $validated->errors()->all()[0];
+                return response()->json([
+                    'status' => 'false',
+                    'message' => $error,
+                    'data' => []
+                ], 422);
+            } else {
+                $address = new Address;
+                $address->user_id = $user_id;
+                $address->description = $request->description;
+                $address->province = $request->province;
+                $address->city = $request->city;
+                $result = $address->save();
+                if ($result) {
+                    return response()->json([
+                        'result' => 'Address added successfully.'
+                    ], 200);
+                }
+            }
+        } catch (\Exception $e) {
             return response()->json([
-                'result' => 'Address added successfully.'
-            ], 200);
-        } else {
-            return response()->json([
-                'result' => 'An error has occured.'
-            ], 401);
+                'status' => 'false',
+                'message' => $e->getMessage(),
+                'data' => []
+            ], 500);
         }
     }
 
     public function update(Request $request, $id)
     {
-        $user_id = Auth::user()->id;
-        $address = Address::find($id);
-        $address->user_id = $user_id;
-        $address->description = $request->description;
-        $address->province = $request->province;
-        $address->city = $request->city;
-        $result = $address->update();
-        if ($result) {
+        try {
+            $user_id = Auth::user()->id;
+            $validated = Validator::make($request->all(), [
+                'description' => 'required|string|max:255',
+                'province' => 'required|string|max:255',
+                'city' => 'required|string|max:255'
+            ]);
+
+            if ($validated->fails()) {
+                $error = $validated->errors()->all()[0];
+                return response()->json([
+                    'status' => 'false',
+                    'message' => $error,
+                    'data' => []
+                ], 422);
+            } else {
+                $address = Address::find($id);
+                $address->user_id = $user_id;
+                $address->description = $request->description;
+                $address->province = $request->province;
+                $address->city = $request->city;
+                $result = $address->update();
+                if ($result) {
+                    return response()->json([
+                        'result' => 'Address updated successfully.'
+                    ], 200);
+                }
+            }
+        } catch (\Exception $e) {
             return response()->json([
-                'result' => 'Address updated successfully.'
-            ], 200);
-        } else {
-            return response()->json([
-                'result' => 'An error has occured.'
-            ], 401);
+                'status' => 'false',
+                'message' => $e->getMessage(),
+                'data' => []
+            ], 500);
         }
     }
 
